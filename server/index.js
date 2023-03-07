deea5b7a2e4d2897b378c4198e42e9af64150f00
adriandamii <adriandamii@yahoo.com>
2023-03-07 17:24:46 +0200

    changed the origin

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json({}));
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://mq-market-classic.onrender.com'],
+    origin: ['http://localhost:3000', 'https://market-place-project.onrender.com'],
     methods: "GET, POST, PUT, DELETE"
   })
 );
